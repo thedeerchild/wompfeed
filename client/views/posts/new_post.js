ae8e45844dcbf4b8d7d4234b4586208953c6e5b8
Eric Morris <elm232@cornell.edu>
2014-07-26 15:40:53 -0700

    Implement initial post creation

---
+++ client/views/posts/new_post.js
@@ -0,0 +1,19 @@
+Template.newPost.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      title: $(e.target).find('[name=title]').val(),
+      content: $(e.target).find('[name=content]').val(),
+      anonymous: $(e.target).find('[name=anonymous]').val()
+    }
+
+    Meteor.call('createPost', post, function(error, id) {
+      if (error) {
+        alert(error.reason);
+      } else {
+        Router.go('home');//, {_id: id});
+      }
+    });
+  }
+});
---
+++ collections/posts.js
@@ -0,0 +1,26 @@
+Posts = new Meteor.Collection('posts');
+
+Meteor.methods({
+  createPost: function(postAttrs) {
+    var user = Meteor.user();
+    
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to post new stories");
+
+    if (!postAttrs.title)
+      throw new Meteor.Error(422, 'Please fill in a headline');
+
+    if (!postAttrs.content || postAttrs.content.length < 1)
+      throw new Meteor.Error(422, 'Please fill in some content');
+
+    var post = _.extend(_.pick(postAttrs, 'title', 'content', 'anonymous'), {
+      userId: user._id, 
+      author: postAttrs.anonymous ? 'Anonymous' : user.profile.name, 
+      submitted: new Date().getTime()
+    });
+
+    var postId = Posts.insert(post);
+
+    return postId;
+  }
+});
