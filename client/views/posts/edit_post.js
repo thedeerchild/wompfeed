e2b15a1204f0b9564d3f505b8b89ebe1645fc58f
Eric Morris <elm232@cornell.edu>
2014-07-28 22:20:18 -0700

    Display all the fields properly in the edit view for a post

--- client/views/posts/edit_post.js
+++ client/views/posts/edit_post.js
@@ -1,3 +1,9 @@
+Template.editPost.helpers({
+  post: function() {
+    return Posts.findOne(this._id);
+  }
+});
+
 Template.editPost.rendered = function() {
   // window.editor = true;
 }
--- client/views/posts/edit_post_form.js
+++ client/views/posts/edit_post_form.js
@@ -1,3 +1,22 @@
+Template.editPostForm.helpers({
+  titleText: function() {
+    var post = Posts.findOne(this._id);
+    if (!!post) {
+      return post.title;
+    } else {
+      return 'Click to Add Title'
+    }
+  },
+  headerImageUrl: function() {
+    var post = Posts.findOne(this._id);
+    if (!!post) {
+      return post.headerImage;
+    } else {
+      return 'http://placehold.it/500x500&text=%20'
+    }
+  }
+});
+
 Template.editPostForm.rendered = function() {
   $('textarea:visible').expanding();
   window.editor = new Editor();
