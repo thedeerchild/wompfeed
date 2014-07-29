4cdbce02c3f41ce7c1239e48587d6f5df01ea967
Eric Morris <elm232@cornell.edu>
2014-07-28 22:31:49 -0700

    Hook up edit method on server

--- client/views/posts/edit_post.js
+++ client/views/posts/edit_post.js
@@ -4,6 +4,29 @@ Template.editPost.helpers({
   }
 });
 
+Template.editPost.events({
+  'click .submit-form': function(e, t) {
+    e.preventDefault();
+
+    var post = {
+      _id: t.data._id,
+      title: $('[name=title]').text(),
+      headerImage: $('[hero-background]').attr('src'),
+      introText: $('[name=introText]').val(),
+      content: window.editor.save(),
+      anonymous: $('[name=anonymous]').is(':checked')    
+    }
+
+    Meteor.call('editPost', post, function(error, id) {
+      if (error) {
+        alert(error.reason);
+      } else {
+        Router.go('showPost', {_id: id});
+      }
+    });
+  }
+});
+
 Template.editPost.rendered = function() {
   // window.editor = true;
 }
--- collections/posts.js
+++ collections/posts.js
@@ -21,5 +21,27 @@ Meteor.methods({
     var postId = Posts.insert(post);
 
     return postId;
+  },
+  editPost: function(postAttrs) {
+    var user = Meteor.user(),
+        postId = postAttrs._id,
+        post = Posts.findOne(postId);
+    
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to post new stories");
+
+    if (!ownsDoc(user._id, post))
+      throw new Meteor.Error(401, "Um...you don't seem to own this post");
+
+    if (!postAttrs.title)
+      throw new Meteor.Error(422, 'Please fill in a headline');
+
+    var post = _.extend(_.pick(postAttrs, 'title', 'headerImage', 'introText', 'content', 'anonymous'), {
+      updated: new Date().getTime()
+    });
+
+    Posts.update(postId, {$set:post});
+
+    return postId;
   }
 });
